851b57d6aaec3c0b4fb7718bd091ebd2980a7c74
Ekiles225 <ekiles07@outlook.com>
2025-01-02 19:54:01 -0900

    Actualizacion

--- src/app/perfil-usuario/perfil-usuario.page.ts
+++ src/app/perfil-usuario/perfil-usuario.page.ts
@@ -2,14 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
-  IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonList, IonItem, IonIcon, IonLabel, IonButton, IonCard,
-  IonCardHeader, IonInput, IonRow, IonCol, IonListHeader, IonBadge, IonCardContent
-} from '@ionic/angular/standalone';
+  IonContent, IonList, IonItem, IonIcon, IonLabel, IonInput, IonAvatar, IonInfiniteScroll,
+  IonInfiniteScrollContent, InfiniteScrollCustomEvent, IonButton, IonPopover } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
 import { UserService } from '../Servicios/user.service';
 import { PersonService } from '../Servicios/person.service';
-
-
 
 
 @Component({
@@ -17,10 +14,8 @@ import { PersonService } from '../Servicios/person.service';
   templateUrl: './perfil-usuario.page.html',
   styleUrls: ['./perfil-usuario.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
-    IonButtons, IonList, IonItem, IonIcon, IonLabel, IonButton,
-    IonCard, IonCardHeader, RouterLink, IonRow, IonCol, IonListHeader, IonCardContent,
-    IonBadge, IonInput
+  imports: [IonPopover, IonButton, IonInfiniteScroll, IonAvatar, IonContent, CommonModule, FormsModule, IonList, IonItem, IonIcon, IonLabel,
+     RouterLink, IonInput, IonInfiniteScrollContent
   ]
 })
 
@@ -31,7 +26,13 @@ export class PerfilUsuarioPage implements OnInit {
   profile: any;
   personid: any;
   editDatos: boolean = true;
-  Guardar: any
+  playlists: any[] = [
+    { name: 'Playlist 1', image: 'assets/image.png' },
+    { name: 'Playlist 2', image: 'assets/principalMusica.jpg' },
+    // Agrega más playlists según sea necesario
+  ];
+
+  isMenuOpen: boolean[] = [];
 
   constructor(private usuarioService: UserService, private personService: PersonService) {
     this.personid = localStorage.getItem('id');
@@ -39,14 +40,16 @@ export class PerfilUsuarioPage implements OnInit {
 
   ngOnInit() {
     this.viewProfile();
+    console.log('Playlists cargadas:', this.playlists);
   }
 
 
+
+
+  // cosas de la edicion 
   editPerfil() {
     this.editDatos = false;
   }
-
-
   viewProfile() {
     this.usuarioService.getOneUser(this.personid).subscribe({
       next: (data: any) => {
@@ -56,28 +59,44 @@ export class PerfilUsuarioPage implements OnInit {
         console.log("error");
       }
     })
-
   }
-
   updatePerson() {
-
     if (this.editDatos) return; // Si no se está editando, no hacer nada
-
     const id = this.profile.user.person.id; // Obteniendo el id dinámicamente
     const nombreUsuario = this.profile.user.person.nombreUsuario;
     const descripcion = this.profile.user.person.descripcion;
-
-
     this.personService.updatePerson(id, nombreUsuario, descripcion).subscribe({
       next: (data: any) => {
         this.profile = data;
         console.log('Perfil actualizado con éxito:', data);
-        debugger
       },
       error: (error: any) => {
         console.error('Error al actualizar el perfil:', error);
-        debugger
       }
     })
   }
+
+  // muestra de los playList 
+
+  openMenu(index: number) {
+    this.isMenuOpen[index] = true;
+  }
+
+  closeMenu(index: number) {
+    this.isMenuOpen[index] = false;
+  }
+
+  editPlaylist(index: number) {
+    // Lógica para editar la playlist
+    console.log('Editar playlist:', this.playlists[index]);
+    this.closeMenu(index);
+  }
+
+  deletePlaylist(index: number) {
+    // Lógica para eliminar la playlist
+    console.log('Eliminar playlist:', this.playlists[index]);
+    this.playlists.splice(index, 1);
+    this.closeMenu(index);
+  }
+
 }
